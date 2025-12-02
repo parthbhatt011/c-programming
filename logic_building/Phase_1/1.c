ace193db3168d7dc3e4e5968e397015ab6810a7d
PARTH BHATT <parthbhatt011@gmail.com>
2025-12-02 19:08:51 +0530

    ..

---
+++ logic_building/Phase_1/1.c
@@ -0,0 +1,12 @@
+//Take a number and print whether it’s positive, negative, or zero. 
+
+#include<stdio.h>
+
+int main(){
+        double x;
+        printf("enter a number: ");
+        scanf("%d", &x);
+        
+
+    return  0;
+}
