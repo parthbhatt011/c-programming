968be70a5e50000157f9551f603a7bd62229238a
PARTH BHATT <parthbhatt011@gmail.com>
2025-12-05 18:06:24 +0530

    ..

--- logic_building/Phase_1/level_1/3.c
+++ logic_building/Phase_1/level_1/3.c
@@ -1,1 +1,21 @@
-//Check if a number is divisible by 5
+// Check if a number is divisible by 5
+
+#include <stdio.h>
+
+int isDivisibleBy5(int x) {
+    return (x % 5 == 0); 
+}
+
+int main() {
+    int x;
+    printf("Enter the number to check: ");
+    scanf("%d", &x);
+
+    if (isDivisibleBy5(x)) {
+        printf("The number is divisible by 5\n");
+    } else {
+        printf("The number is not divisible by 5\n");
+    }
+
+    return 0;
+}
