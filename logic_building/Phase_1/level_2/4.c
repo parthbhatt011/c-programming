48a46065dfad75aa30fd0b1162910d3ef6007d79
PARTH BHATT <parthbhatt011@gmail.com>
2025-12-10 19:06:44 +0530

    ..

--- logic_building/Phase_1/level_2/4.c
+++ logic_building/Phase_1/level_2/4.c
@@ -4,16 +4,21 @@
 
 int main() {
     int a, b;
-    printf("Enter two integers: ");
+
+    printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
 
+    // Zero check to avoid division by zero
     if (a == 0 || b == 0) {
-        printf("Zero cannot be used for checking multiples.\n");
-    } else if (a % b == 0) {
+        printf("Multiples involving zero are undefined for division.\n");
+    }
+    else if (a % b == 0) {
         printf("%d is a multiple of %d\n", a, b);
-    } else if (b % a == 0) {
+    }
+    else if (b % a == 0) {
         printf("%d is a multiple of %d\n", b, a);
-    } else {
+    }
+    else {
         printf("Neither number is a multiple of the other.\n");
     }
 
