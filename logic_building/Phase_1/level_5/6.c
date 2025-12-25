41383f658a5bc8ea91e0554effc4995e54c0d042
PARTH BHATT <parthbhatt011@gmail.com>
2025-12-25 19:28:11 +0530

    ..

--- logic_building/Phase_1/level_5/6.c
+++ logic_building/Phase_1/level_5/6.c
@@ -1,1 +1,18 @@
 //Take three numbers and check if they are in geometric progression.
+
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a != 0 && (b * b) == (a * c)) {
+        printf("The numbers are in Geometric Progression (GP).\n");
+    } else {
+        printf("The numbers are NOT in Geometric Progression.\n");
+    }
+
+    return 0;
+}
