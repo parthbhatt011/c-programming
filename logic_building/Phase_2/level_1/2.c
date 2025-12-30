dbc319a2fd0ead726d1980e78b522aab6bd0e025
PARTH BHATT <parthbhatt011@gmail.com>
2025-12-30 10:51:18 +0530

    ..

---
+++ logic_building/Phase_2/level_1/2.c
@@ -0,0 +1,13 @@
+//Print all even numbers between 1 and 100. 
+
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    for (i = 1; i <= 10; i++) {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
